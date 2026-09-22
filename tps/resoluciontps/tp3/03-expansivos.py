db62590960536ef812bc17b898a35bac0538ef21
Hernan Nicolaus <hernannicolaus@gmail.com>
2026-09-22 10:05:58 -0300

    comentarios para no olvidar

--- tps/resoluciontps/tp3/03-expansivos.py
+++ tps/resoluciontps/tp3/03-expansivos.py
@@ -2,23 +2,28 @@ import unittest
 import numpy as np
 
 def expand(arreglo: np.ndarray) -> np.ndarray:
+    # esta funcion cuenta cuantas veces se repite cada numero seguido
     resultado = []
-    cifra_actual = arreglo[0]
+    cifraActual = arreglo[0]
     contador = 1
     for i in range(1, len(arreglo)):
-        if arreglo[i] == cifra_actual:
+        if arreglo[i] == cifraActual:
+            # es el mismo numero que el anterior, sigo contando
             contador += 1
         else:
+            # cambio el numero, guardo lo que iba contando y arranco de nuevo
             resultado.append(contador)
-            resultado.append(cifra_actual)
-            cifra_actual = arreglo[i]
+            resultado.append(cifraActual)
+            cifraActual = arreglo[i]
             contador = 1
+    # el ultimo grupo no se guarda en el for, asi que lo agrego aca
     resultado.append(contador)
-    resultado.append(cifra_actual)
+    resultado.append(cifraActual)
     return np.array(resultado)
 
 
 def list2num(arreglo: np.ndarray) -> int:
+    # Convierte un array de digitos en el numero entero que representan.
     numero = 0
     for cifra in arreglo:
         numero = numero * 10 + cifra
